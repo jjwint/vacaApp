43857dd75d112776d1067da58eb723342666ae43
Josh Klimaszewski <joshklimaszewski@Joshs-MacBook-Pro.local>
2018-03-05 15:28:42 -0500

    started more API stuff

--- components/cityView.component.js
+++ components/cityView.component.js
@@ -1,8 +1,17 @@
 (function () {
     var cityView = {
         templateUrl: "partials/cityView.html",
-        controller: function (CityService) {
+        controller: function () {
             var $ctrl = this;
+            $ctrl.initMap() = function () {
+                $ctrl.map = new google.maps.Map(document.getElementById('map'), {
+                    center: {
+                        lat: 42.3359240,
+                        lng: -83.0497190
+                    },
+                    zoom: 15
+                });
+            }
         }
     };
     angular
--- services/APIService.service.js
+++ services/APIService.service.js
@@ -1,7 +1,24 @@
 (function () {
-    function APIService () {
+    function APIService() {
+       
         return {
-            
+            initMap: initMap,
+            getMap: getMap
+        }
+        var map;
+        
+
+        function initMap() {
+            map = new google.maps.Map(document.getElementById('map'), {
+                center: {
+                    lat: 42.3359240,
+                    lng: -83.0497190
+                },
+                zoom: 15
+            });
+        }
+        function getMap() {
+            return map;
         }
     }
     angular
