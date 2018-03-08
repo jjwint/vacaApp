7711f0319d0765bdff89b26d41fe46611eb12440
D'Lorean Bell <dloreanbell@gmail.com>
2018-03-08 16:50:07 -0500

    changes

--- services/CityService.service.js
+++ services/CityService.service.js
@@ -15,52 +15,53 @@
             getCityCuisine: getCityCuisine,
             getCitySport: getCitySport,
             getCityEntertainment: getCityEntertainment,
-            
+
         }
 
         function checkLogin(newObj, data) {
-            
+
             cityRegion = newObj.region;
             cityCuisine = newObj.cuisine;
             citySport = newObj.sport;
             cityEntertainment = newObj.entertainment;
 
-           
+
             possibleCities.length = 0;
             for (i = 0; i <= 7; i++) {
                 var thisCity = data[i];
                 thisCity.counter = 0;
-                
+
                 // TODO change this if so that it includes all cities if no region picked.
-                // if the city is in the right region, add it to the list and rate it.
-                if(cityRegion === thisCity.region){
+                // if the city is in the right region, add it to the list and rate it.  
+
+
+
+                if (cityRegion === thisCity.region) {
                     // add it to the list
                     possibleCities.push(thisCity);
                     console.log(possibleCities);
                     // rate it... a point for each
                     if (thisCity.cuisine.includes(newObj.cuisine) === true) {
-
-                            thisCity.counter++;
-                
+                        thisCity.counter++;
                     }
-                
+
                     if (thisCity.sports.includes(newObj.sport) === true) {
 
                         thisCity.counter++;
-                    
+
                     }
 
                     if (thisCity.entertainment.includes(newObj.entertainment) === true) {
 
                         thisCity.counter++;
-                    
+
                     }
 
-                    console.log("region matched", thisCity);
+                    console.log("counter", thisCity.counter);
                 }
-              
+
             }
-            
+
         }
 
         function getLibrary() {
@@ -76,8 +77,8 @@
 
         function saveCityObj(city) {
             document.getElementById("appInput").style.display = "none";
-            displayCity = city; 
-            
+            displayCity = city;
+
 
 
         }
@@ -97,7 +98,7 @@
         function getCityEntertainment() {
             return cityEntertainment;
         }
-        
+
 
 
 
