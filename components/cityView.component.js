f4421a479481b3bc458b7f1f6d6d8a33b7ae0adf
Josh Klimaszewski <joshklimaszewski@Joshs-MacBook-Pro.local>
2018-03-08 15:11:47 -0500

    cityview

--- components/cityView.component.js
+++ components/cityView.component.js
@@ -11,19 +11,19 @@
             var $ctrl = this;
             $ctrl.thisCity = CityService.getCityObj();
             $ctrl.$onInit = function () {
-               
+
                 $ctrl.thisCitySport = CityService.getCitySport();
                 $ctrl.thisCityCuisine = CityService.getCityCuisine();
                 $ctrl.thisCityEntertainment = CityService.getCityEntertainment();
-                
+
                 $ctrl.thisCityLoc = new google.maps.LatLng($ctrl.thisCity.latitude, $ctrl.thisCity.longitude);
-                
+
                 $ctrl.map = new google.maps.Map(document.getElementById("map"), {
                     center: $ctrl.thisCityLoc,
                     zoom: 13
-                    
+
                 });
-                console.log($ctrl.map)
+                console.log($ctrl.map);
                 $ctrl.service = new google.maps.places.PlacesService($ctrl.map);
 
                 $ctrl.request = {
@@ -37,10 +37,8 @@
                     if (status == google.maps.places.PlacesServiceStatus.OK) {
                         $ctrl.results = results;
                         for (var i = 0; i < results.length; i++) {
-                            // console.log(results[i])
                             var place = results[i];
                             $ctrl.createMarker(results[i]);
-                            // $ctrl.createInfoWindow(results[i]);
                         }
                     }
                 }
@@ -53,13 +51,14 @@
                     var infoWindow = new google.maps.InfoWindow({
                         content: "<p>hello</p>"
                     });
-                    marker.addListener('click', function() {
+                    marker.addListener('click', function () {
                         infoWindow.setContent(place.name);
                         infoWindow.open($ctrl.map, marker)
                     })
                 }
                 $ctrl.service.textSearch($ctrl.request, $ctrl.callback);
             };
+            
         }
     };
 
