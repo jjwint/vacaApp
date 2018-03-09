18dc47b80f3526b6d07e3e8fdb0da3c1b12ee35a
Josh Klimaszewski <joshklimaszewski@Joshs-MacBook-Pro.local>
2018-03-08 20:15:32 -0500

    merge conflict

--- services/CityService.service.js
+++ services/CityService.service.js
@@ -15,10 +15,7 @@
             getCityCuisine: getCityCuisine,
             getCitySport: getCitySport,
             getCityEntertainment: getCityEntertainment,
-<<<<<<< HEAD
-=======
-            reset: reset
->>>>>>> 82cb19e260973eec881454efccf73fc0e47c1786
+            // reset: reset
 
         }
 
@@ -36,27 +33,16 @@
                 thisCity.counter = 0;
 
                 // TODO change this if so that it includes all cities if no region picked.
-<<<<<<< HEAD
-                // if the city is in the right region, add it to the list and rate it.  
-
-
-
-=======
                 // if the city is in the right region, add it to the list and rate it.
->>>>>>> 82cb19e260973eec881454efccf73fc0e47c1786
                 if (cityRegion === thisCity.region) {
                     // add it to the list
                     possibleCities.push(thisCity);
                     console.log(possibleCities);
                     // rate it... a point for each
                     if (thisCity.cuisine.includes(newObj.cuisine) === true) {
-<<<<<<< HEAD
-                        thisCity.counter++;
-=======
 
                         thisCity.counter++;
 
->>>>>>> 82cb19e260973eec881454efccf73fc0e47c1786
                     }
 
                     if (thisCity.sports.includes(newObj.sport) === true) {
@@ -113,20 +99,17 @@
             return cityEntertainment;
         }
 
-<<<<<<< HEAD
-=======
-        function reset() {
-            possibleCities = [];
-            displayCity = {};
-            cityRegion = "";
-            cityCuisine = "";
-            citySport = "";
-            cityEntertainment = "";
-            document.getElementById("appInput").style.display = "block";
+        // function reset() {
+        //     possibleCities = [];
+        //     displayCity = {};
+        //     cityRegion = "";
+        //     cityCuisine = "";
+        //     citySport = "";
+        //     cityEntertainment = "";
+        //     document.getElementById("appInput").style.display = "block";
 
-        }
+        // }
 
->>>>>>> 82cb19e260973eec881454efccf73fc0e47c1786
 
 
 
