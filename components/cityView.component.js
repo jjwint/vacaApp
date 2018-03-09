f8d5ebad66fac507ba282d2af500e2e1e7d86501
D'Lorean Bell <dloreanbell@gmail.com>
2018-03-09 16:26:38 -0500

    api

--- components/cityView.component.js
+++ components/cityView.component.js
@@ -21,13 +21,27 @@
                     zoom: 13
                 });
                 // make a search request to API
-                $ctrl.service = new google.maps.places.PlacesService($ctrl.map);
+               
                 // create request object with search parameters
+
+
+            };
+            $ctrl.searchCuisine = function () {
+                console.log($ctrl.thisCity);
+                console.log($ctrl.thisCityCuisine);
+                
+                $ctrl.thisCityLoc = new google.maps.LatLng($ctrl.thisCity.latitude, $ctrl.thisCity.longitude);
+                $ctrl.map = new google.maps.Map(document.getElementById("map"), {
+                    center: $ctrl.thisCityLoc,
+                    zoom: 13
+                });
+                $ctrl.thisCityLoc = new google.maps.LatLng($ctrl.thisCity.latitude, $ctrl.thisCity.longitude);
+                $ctrl.service = new google.maps.places.PlacesService($ctrl.map);
                 $ctrl.request = {
                     location: $ctrl.thisCityLoc,
                     radius: '100',
-                    query: $ctrl.thisCityCuisine, //change this for buttons
-                    type: ['restaurant'] //change this for buttons
+                    query: $ctrl.thisCityCuisine + " restaurant", //change this for buttons
+                    type: ['restaraunt'] //change this for buttons
                 };
                 $ctrl.callback = function (results, status) {
                     if (status == google.maps.places.PlacesServiceStatus.OK) {
@@ -48,12 +62,97 @@
                         content: "<p>hello</p>"
                     });
                     marker.addListener('click', function () {
-                        infoWindow.setContent(place.name);  //can be more robust
+                        infoWindow.setContent(place.name); //can be more robust
                         infoWindow.open($ctrl.map, marker)
                     })
                 }
                 $ctrl.service.textSearch($ctrl.request, $ctrl.callback);
-            };
+            }
+
+
+
+            $ctrl.searchEntertainment = function () {
+                $ctrl.thisCityLoc = new google.maps.LatLng($ctrl.thisCity.latitude, $ctrl.thisCity.longitude);
+                $ctrl.service = new google.maps.places.PlacesService($ctrl.map);
+                $ctrl.map = new google.maps.Map(document.getElementById("map"), {
+                    center: $ctrl.thisCityLoc,
+                    zoom: 13
+                });
+                $ctrl.request = {
+                    location: $ctrl.thisCityLoc,
+                    radius: '100',
+                    query: $ctrl.thisCityEntertainment //change this for buttons
+                    // type: ['stadium'] //change this for buttons
+                };
+                $ctrl.callback = function (results, status) {
+                    if (status == google.maps.places.PlacesServiceStatus.OK) {
+                        $ctrl.results = results;
+                        for (var i = 0; i < results.length; i++) {
+                            var place = results[i];
+                            console.log(place)
+                            $ctrl.createMarker(results[i]);
+                        }
+                    }
+                }
+                $ctrl.createMarker = function (place) {
+                    var marker = new google.maps.Marker({
+                        map: $ctrl.map,
+                        position: place.geometry.location
+                    });
+                    var infoWindow = new google.maps.InfoWindow({
+                        content: "<p>hello</p>"
+                    });
+                    marker.addListener('click', function () {
+                        infoWindow.setContent(place.name); //can be more robust
+                        infoWindow.open($ctrl.map, marker)
+                    })
+                }
+                $ctrl.service.textSearch($ctrl.request, $ctrl.callback);
+            }
+
+            $ctrl.searchSport = function () {
+                $ctrl.thisCityLoc = new google.maps.LatLng($ctrl.thisCity.latitude, $ctrl.thisCity.longitude);
+                $ctrl.service = new google.maps.places.PlacesService($ctrl.map);
+                $ctrl.map = new google.maps.Map(document.getElementById("map"), {
+                    center: $ctrl.thisCityLoc,
+                    zoom: 13
+                });
+                $ctrl.request = {
+                    location: $ctrl.thisCityLoc,
+                    radius: '100',
+                    query: $ctrl.thisCitySport, //change this for buttons
+                    type: ['stadium'] //change this for buttons
+                };
+                $ctrl.callback = function (results, status) {
+                    if (status == google.maps.places.PlacesServiceStatus.OK) {
+                        $ctrl.results = results;
+                        for (var i = 0; i < results.length; i++) {
+                            var place = results[i];
+                            console.log(place)
+                            $ctrl.createMarker(results[i]);
+                        }
+                    }
+                }
+                $ctrl.createMarker = function (place) {
+                    var marker = new google.maps.Marker({
+                        map: $ctrl.map,
+                        position: place.geometry.location
+                    });
+                    var infoWindow = new google.maps.InfoWindow({
+                        content: "<p>hello</p>"
+                    });
+                    marker.addListener('click', function () {
+                        infoWindow.setContent(place.name); //can be more robust
+                        infoWindow.open($ctrl.map, marker)
+                    })
+                }
+                $ctrl.service.textSearch($ctrl.request, $ctrl.callback);
+            }
+
+
+
+
+
         }
     };
 
