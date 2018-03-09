bf07bf151cf8d1c077980a9954f3ff824457603b
D'Lorean Bell <dloreanbell@gmail.com>
2018-03-08 22:33:13 -0500

    changes

--- services/CityService.service.js
+++ services/CityService.service.js
@@ -15,10 +15,6 @@
             getCityCuisine: getCityCuisine,
             getCitySport: getCitySport,
             getCityEntertainment: getCityEntertainment,
-<<<<<<< HEAD
-=======
-            reset: reset
->>>>>>> 82cb19e260973eec881454efccf73fc0e47c1786
 
         }
 
@@ -36,27 +32,47 @@
                 thisCity.counter = 0;
 
                 // TODO change this if so that it includes all cities if no region picked.
-<<<<<<< HEAD
-                // if the city is in the right region, add it to the list and rate it.  
-
-
-
-=======
                 // if the city is in the right region, add it to the list and rate it.
->>>>>>> 82cb19e260973eec881454efccf73fc0e47c1786
-                if (cityRegion === thisCity.region) {
+
+                if (cityRegion !== thisCity.region) {
+
+                    // console.log("yes");
+                    if (thisCity.cuisine.includes(newObj.cuisine) === true) {
+
+                        thisCity.counter++;
+
+                    }
+
+                    if (thisCity.sports.includes(newObj.sport) === true) {
+
+                        thisCity.counter++;
+
+                    }
+
+                    if (thisCity.entertainment.includes(newObj.entertainment) === true) {
+
+                        thisCity.counter++;
+
+                    }
+
+                    // console.log("counter", thisCity.counter);
+
+                    if (thisCity.counter > 1) {
+
+                        possibleCities.push(thisCity);
+
+                    }
+
+                } else if (cityRegion === thisCity.region) {
                     // add it to the list
+                    // possibleCities.length = 0;
                     possibleCities.push(thisCity);
-                    console.log(possibleCities);
+                    // console.log(possibleCities);
                     // rate it... a point for each
                     if (thisCity.cuisine.includes(newObj.cuisine) === true) {
-<<<<<<< HEAD
-                        thisCity.counter++;
-=======
 
                         thisCity.counter++;
 
->>>>>>> 82cb19e260973eec881454efccf73fc0e47c1786
                     }
 
                     if (thisCity.sports.includes(newObj.sport) === true) {
@@ -73,6 +89,7 @@
 
                     console.log("counter", thisCity.counter);
                 }
+
 
             }
 
@@ -113,8 +130,6 @@
             return cityEntertainment;
         }
 
-<<<<<<< HEAD
-=======
         function reset() {
             possibleCities = [];
             displayCity = {};
@@ -126,7 +141,6 @@
 
         }
 
->>>>>>> 82cb19e260973eec881454efccf73fc0e47c1786
 
 
 
