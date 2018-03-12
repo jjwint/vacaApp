d906872503c7acabf1cd25f518f946cd2b08e003
Jacqueline Winterbottom <jjwint90@gmail.com>
2018-03-12 15:14:20 -0400

    merge

--- services/CityService.service.js
+++ services/CityService.service.js
@@ -16,8 +16,6 @@
             getCityCuisine: getCityCuisine,
             getCitySport: getCitySport,
             getCityEntertainment: getCityEntertainment,
-            getCityName: getCityName,
-
             reset: reset,
         }
 
