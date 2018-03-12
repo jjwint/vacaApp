924a7b0d1e29b1a4c4087719c49b4dba6be42fc8
D'Lorean Bell <dloreanbell@gmail.com>
2018-03-12 09:33:22 -0400

    api changes

--- components/cityView.component.js
+++ components/cityView.component.js
@@ -8,6 +8,9 @@
         controller: function (CityService) {
             var $ctrl = this;
             // API code won't fire until cityView is initialized
+            
+            
+            
             $ctrl.$onInit = function () {
                 // import data from service
                 $ctrl.thisCitySport = CityService.getCitySport();
@@ -21,32 +24,33 @@
                     zoom: 13
                 });
                 // make a search request to API
-               
-                // create request object with search parameters
+
+       
+
 
 
             };
             $ctrl.searchCuisine = function () {
                 console.log($ctrl.thisCity);
                 console.log($ctrl.thisCityCuisine);
-                
+
                 $ctrl.thisCityLoc = new google.maps.LatLng($ctrl.thisCity.latitude, $ctrl.thisCity.longitude);
                 $ctrl.map = new google.maps.Map(document.getElementById("map"), {
                     center: $ctrl.thisCityLoc,
-                    zoom: 13
+                    zoom: 10
                 });
                 $ctrl.thisCityLoc = new google.maps.LatLng($ctrl.thisCity.latitude, $ctrl.thisCity.longitude);
                 $ctrl.service = new google.maps.places.PlacesService($ctrl.map);
                 $ctrl.request = {
                     location: $ctrl.thisCityLoc,
                     radius: '100',
-                    query: $ctrl.thisCityCuisine + " restaurant", //change this for buttons
+                    query: $ctrl.thisCityCuisine + "top restaurant", //change this for buttons
                     type: ['restaraunt'] //change this for buttons
                 };
                 $ctrl.callback = function (results, status) {
                     if (status == google.maps.places.PlacesServiceStatus.OK) {
                         $ctrl.results = results;
-                        for (var i = 0; i < results.length; i++) {
+                        for (var i = 0; i < 5; i++) {
                             var place = results[i];
                             console.log(place)
                             $ctrl.createMarker(results[i]);
@@ -59,10 +63,10 @@
                         position: place.geometry.location
                     });
                     var infoWindow = new google.maps.InfoWindow({
-                        content: "<p>hello</p>"
+
                     });
                     marker.addListener('click', function () {
-                        infoWindow.setContent(place.name); //can be more robust
+                        infoWindow.setContent('<div class ="infoBox"><h3 class="infoName">' + place.name + '</h3>' + '<div class ="rating">Rating: ' + place.rating + '</div>' + place.formatted_address + '</div');
                         infoWindow.open($ctrl.map, marker)
                     })
                 }
@@ -76,22 +80,23 @@
                 $ctrl.service = new google.maps.places.PlacesService($ctrl.map);
                 $ctrl.map = new google.maps.Map(document.getElementById("map"), {
                     center: $ctrl.thisCityLoc,
-                    zoom: 13
+                    zoom: 11
                 });
                 $ctrl.request = {
                     location: $ctrl.thisCityLoc,
-                    radius: '100',
-                    query: $ctrl.thisCityEntertainment //change this for buttons
+                    radius: '150',
+                    query: $ctrl.thisCityEntertainment + " best" //change this for buttons
                     // type: ['stadium'] //change this for buttons
                 };
                 $ctrl.callback = function (results, status) {
                     if (status == google.maps.places.PlacesServiceStatus.OK) {
                         $ctrl.results = results;
-                        for (var i = 0; i < results.length; i++) {
+                        for (var i = 0; i < 6; i++) {
                             var place = results[i];
                             console.log(place)
                             $ctrl.createMarker(results[i]);
                         }
+
                     }
                 }
                 $ctrl.createMarker = function (place) {
@@ -100,12 +105,14 @@
                         position: place.geometry.location
                     });
                     var infoWindow = new google.maps.InfoWindow({
-                        content: "<p>hello</p>"
+
                     });
                     marker.addListener('click', function () {
-                        infoWindow.setContent(place.name); //can be more robust
+                        infoWindow.setContent('<div class ="infoBox"><h3 class="infoName">' + place.name + '</h3>' + '<div class ="rating">Rating: ' + place.rating + '</div>' + place.formatted_address + '</div>');
                         infoWindow.open($ctrl.map, marker)
                     })
+
+
                 }
                 $ctrl.service.textSearch($ctrl.request, $ctrl.callback);
             }
@@ -115,7 +122,7 @@
                 $ctrl.service = new google.maps.places.PlacesService($ctrl.map);
                 $ctrl.map = new google.maps.Map(document.getElementById("map"), {
                     center: $ctrl.thisCityLoc,
-                    zoom: 13
+                    zoom: 9
                 });
                 $ctrl.request = {
                     location: $ctrl.thisCityLoc,
@@ -126,7 +133,7 @@
                 $ctrl.callback = function (results, status) {
                     if (status == google.maps.places.PlacesServiceStatus.OK) {
                         $ctrl.results = results;
-                        for (var i = 0; i < results.length; i++) {
+                        for (var i = 0; i < 3; i++) {
                             var place = results[i];
                             console.log(place)
                             $ctrl.createMarker(results[i]);
@@ -134,17 +141,28 @@
                     }
                 }
                 $ctrl.createMarker = function (place) {
+
+                    var placesList = document.getElementById('places');
                     var marker = new google.maps.Marker({
                         map: $ctrl.map,
                         position: place.geometry.location
+
+
                     });
-                    var infoWindow = new google.maps.InfoWindow({
-                        content: "<p>hello</p>"
-                    });
+                    var infoWindow = new google.maps.InfoWindow();
                     marker.addListener('click', function () {
-                        infoWindow.setContent(place.name); //can be more robust
+                        infoWindow.setContent('<div class ="infoBox"><h3 class="infoName">' + place.name + '</h3>' + '<div class ="rating">Rating: ' + place.rating + '</div>' + place.formatted_address + '</div'); //can be more robust
                         infoWindow.open($ctrl.map, marker)
                     })
+
+                    var li = document.createElement('li');
+                    // $('.places').empty();
+
+                    li.className = "results";
+                    li.textContent = place.name + " " + place.rating;
+                    placesList.appendChild(li);
+
+
                 }
                 $ctrl.service.textSearch($ctrl.request, $ctrl.callback);
             }
